d073d23b3cbc3043c1d47a862fa19872882cf13c
HotoRas <hotoras03@gmail.com>
2024-07-27 15:53:39 +0900

    Leave what backend src use and remove others
    Backend tests? Do on nodejs

--- misskey-rs/src/lib.rs
+++ misskey-rs/src/lib.rs
@@ -1,26 +1,8 @@
 #![feature(never_type)]
 #![allow(non_snake_case, non_camel_case_types)]
 
-mod api_types;
-pub use api_types::Endpoints;
-
-mod streaming;
-pub use streaming::{Connection as ChannelConnection, Stream};
-
-mod streaming_types;
-pub use streaming_types::Channels;
-
-pub mod acct;
-pub use acct::Acct;
-
 mod consts;
 pub use consts::{
     followersVisibilities, followingVisibilities, moderationLogTypes, mutedNoteReasons,
     noteVisibilities, notificationTypes, permissions,
 };
-
-pub mod api;
-pub mod entities;
-
-// temp
-mod autogen;
